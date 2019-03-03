d666cd4cdf6958f14bd92ec0fac220ae9dafd9b0
Hamza Çakmak <reelaktor@gmail.com>
2019-03-03 14:06:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harf_notu_man_op_li
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Harf notu Mantıksal operatör ile çözümü...
+            //Yaklaşık değerler verilmiştir
+            Console.WriteLine("0 ile 100 Arasında Deger Giriniz");
+            int not = Convert.ToInt32(Console.ReadLine());
+            if (not <= 100 && not >= 85)
+                Console.WriteLine("AA");
+            if (not <= 84 && not >= 75)
+                Console.WriteLine("BA");
+            if (not <= 74 && not >= 65)
+                Console.WriteLine("BB");
+            if (not <= 64 && not >= 60)
+                Console.WriteLine("CB");
+            if (not <= 59 && not >= 50)
+                Console.WriteLine("CC");
+            if (not <=49 && not >= 0)
+                Console.WriteLine("FF");
+        }
+    }
+}
